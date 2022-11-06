b997176b893625b072b1313a05e4fd3837886ed2
EC2 Default User <ec2-user@ip-172-31-6-20.ap-northeast-1.compute.internal>
2022-11-06 11:00:26 +0000

    解答を参考にレビュー実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,13 +13,13 @@ import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
 import "../stylesheets/application"
 
+Rails.start()
+Turbolinks.start()
+ActiveStorage.start()
+
 import Raty from "raty.js"
 window.raty = function(elem,opt) {
   let raty =  new Raty(elem,opt)
   raty.init();
   return raty;
 }
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_05_105127) do
+ActiveRecord::Schema.define(version: 2022_11_06_105306) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_11_05_105127) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "impressions_count", default: 0
+    t.float "rate"
   end
 
   create_table "entries", force: :cascade do |t|
